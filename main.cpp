bbac4b5ee743ca96002c02986d9ae3de29d01cbf
Kenneth Rungstroem Larsen <rungstroem@hotmail.com>
2020-03-23 14:42:42 +0100

    Removed looping behaviour in main

--- main.cpp
+++ main.cpp
@@ -1,107 +1,163 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <iostream>
+#include <fstream>
 
 const int SIZE = 1024;          // Size of Buffers
 char receiveBuff[SIZE];         // Buffer to send to the server
 char sendBuff[SIZE];            // Buffer to receive from server
 char pasvBuff[] = "pasv";       // Buffer to see if PASV Command was entered
 char quitBuff[] = "QUIT";       // Buffer to see if QUIT Command was entered
-char pasvMessage[100];          // String for PASV information
 char userLogin[] = "USER ";
 char passLogin[] = "PASS ";
 char username[] = "ftpuser";
 char password[] = "12345678";
+char trfType[] = "TYPE I";
+char getBuff[] = "GET ";
+char fileName[] = "arnbcfg.xml";
 
 #define FTP_PORT 21
 #define FTP_ADDR 192.168.1.10
 
 int main(){
     int length = 0, i=0;
-    int a1, a2, a3, a4, p1, p2, dataPort;       //PASV Information
+    int a1, a2, a3, a4, p1, p2, dataPort;			//PASV Information
 
-    // Add Server Information
-    struct sockaddr_in server_addr;             // Internet address of server
-    memset(&server_addr, 0, sizeof(server_addr));   // Clear structure
-    server_addr.sin_family = AF_INET;           // Set address typedef
+    //Add Server Information
+    struct sockaddr_in server_addr;					//Internet address of server
+    memset(&server_addr, 0, sizeof(server_addr));   //Clear structure
+    server_addr.sin_family = AF_INET;				//Set address typedef
     memcpy(&server_addr.sin_addr, FTP_ADDR, sizeof(FTP_ADDR));
-    server_addr.sin_port = htons(FTP_PORT);     // Use FTP port
+    server_addr.sin_port = htons(FTP_PORT);			//Use FTP port
 
-    // Create Socket to Connect to FTP Server
-    int commSock = socket(AF_INET, SOCK_STREAM, 0); // Command socket in client
+    //Create control command socket to connect to FTP server
+    int commSock;
+	if((commSock = socket(AF_INET, SOCK_STREAM, 0)) < 0){ //Command socket in client
+		//Implement error handling here
+		std::cout << "failed to create command socket\n";
+	}
+    //Connect to FTP server on port 21
+    if((connect(commSock, (struct sockaddr *)&server_addr, sizeof(server_addr))) < 0){
+		//Implement error handling here
+		std::cout << "failed to connect to socket\n";
+	}
 
-    // Connect to FTP Server on Port 21
-    connect(commSock, (struct sockaddr *)&server_addr, sizeof(server_addr);
+    //Get connection succesful message
+    if((read(commSock, receiveBuff, sizeof(receiveBuff)-1)) < 0){
+		//Implement error handling here
+		std::cout << "failed for read from socket\n";
+	}
+	write(fileno(stdout), receiveBuff, sizeof(receiveBuff)-1);	//Write server response to stdout
+    //Implement error handling on servers response
 
-    // Get Connect Message
-    read(commSock, receiveBuff, sizeof(receiveBuff)-1);
-    
-	// Create login information
+	//Create login information
 	strcat(userLogin, username);
 	strcat(userLogin, "\r\n");
 	strcat(passLogin, password);
 	strcat(passLogin, "\r\n");
 
-	// Write login information to server
+	//Write login information to server
 	memset(receiveBuff, 0, SIZE);
 	write(commSock, userLogin, strlen(userLogin));
-	if(read(commSock, receiveBuff, sizeof(receiveBuff)-1) <0);	//Implement error handling
-
+	if((read(commSock, receiveBuff, sizeof(receiveBuff)-1)) <0){
+		//Implement error handling
+		std::cout << "failed reading from socket after username send\n";
+	}
 	memset(receiveBuff, 0, SIZE);
 	write(commSock, passLogin, strlen(passLogin));
-	if(read(commSock, receiveBuff, sizeof(receiveBuff)-1) <0);	//Implement error handling
-	
+	if((read(commSock, receiveBuff, sizeof(receiveBuff)-1)) <0){
+		//Implement error handling
+		std::cout << "failed reading from socket efter password send\n";
+	}
+	write(fileno(stdout), receiveBuff, sizeof(receiveBuff)-1);	//Write server response to stdout
+	//Implement error handling on server response here
+	//Maybe server responds with "230" - standard response for sufficient credentials to grant access to server
+
 	/*	A valid connection should be established now
 		The commSock is made to send commands to the server */
-	
 
-    do
-    {
-        /* Clear Buffers */
-        memset(receiveBuff, 0, SIZE);
-        memset(sendBuff, 0, SIZE);
+    //Clear buffers
+    memset(receiveBuff, 0, SIZE);
+    memset(sendBuff, 0, SIZE);
 
-        write(fileno(stdout), "Please enter a FTP Command: ", 28);      // Write User Interface
-        read(fileno(stdin), sendBuff, SIZE);                            // Read Command from User
-        send(origSock, sendBuff, strlen(sendBuff) , 0);                 // Send Command to Server
+    if((send(commSock, pasvBuff, strlen(pasvBuff) , 0)) < 0){		//Send pasv command to server
+		//Implement error handling here
+		std::cout << "failed to write PASV command to server\n";
+	}
 
-        read(origSock, receiveBuff, sizeof(receiveBuff) - 1);           // Read Response from Server
-        write(fileno(stdout), receiveBuff, sizeof(receiveBuff) - 1);    // Print Response from Server to screen
+    if((read(commSock, receiveBuff, sizeof(receiveBuff) - 1)) < 0){	//Read Response from Server
+		std::cout << "failed to read from server after PASV command send\n";
+	}
+	write(fileno(stdout), receiveBuff, sizeof(receiveBuff) - 1);    //Print Response from Server to screen
+	//Implement error handling on server response 
 
-        /* If PASV Command was Entered */
-        if (strncmp(sendBuff, pasvBuff, 4) == 0)
-        {
-            sscanf(receiveBuff, "227 Entering Passive Mode (%d,%d,%d,%d,%d,%d)", &a1,&a2,&a3,&a4,&p1,&p2);
-            dataPort = (p1 * 256) + p2;
+    //If "227" response from server upon sending PASV then create data socket - server ready
+    if(strncmp("227", receiveBuff, 3) == 0){
+        sscanf(receiveBuff, "227 Entering Passive Mode (%d,%d,%d,%d,%d,%d)", &a1,&a2,&a3,&a4,&p1,&p2);
+        dataPort = (p1 * 256) + p2;
 
-            struct sockaddr_in servAdr2;                // Internet address of server
-            memset(&servAdr2, 0, sizeof(servAdr2));     // Clear structure
-            servAdr2.sin_family = AF_INET;              // Set address typedef
-            memcpy(&servAdr2.sin_addr, host->h_addr, host->h_length);
-            servAdr.sin_port = htons(dataPort);         // Use FTP port
+        struct sockaddr_in server_addr2;					//Internet address of server
+        memset(&server_addr2, 0, sizeof(server_addr2));     //Clear structure
+        server_addr2.sin_family = AF_INET;						//Set address typedef
+        memcpy(&server_addr2.sin_addr, FTP_ADDR, sizeof(FTP_ADDR));
+        server_addr.sin_port = htons(dataPort);				//Use FTP data port
 
-            /* Create Socket to Connect to FTP Server */
-            int dataSock;                               // Data socket in client
-            if ((dataSock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
-            {
-                perror("Client: generate error");
-                return 3;
-            }
-
-            /* Connect to FTP Server on Data Port */
-            if (connect(dataSock, (struct sockaddr *)&servAdr, sizeof(servAdr)) < 0)
-            {
-                perror("Client: connect error");
-                return 4;
-            }
-
-            read(dataSock, receiveBuff, sizeof(receiveBuff) - 1);
-            write(fileno(stdout), receiveBuff, sizeof(receiveBuff) - 1);
+        //Create data transfer socket
+        int dataSock;                               //Data socket in client
+        if((dataSock = socket(PF_INET, SOCK_STREAM, 0)) < 0){
+			//Implement error handling here
+            std::cout << "failed to create data socket - client error\n";
         }
 
-    } while (strncmp(sendBuff, quitBuff, 4) != 0);      // Go until QUIT Command is entered
+        //Connect to FTP server on dataport
+        if((connect(dataSock, (struct sockaddr *)&server_addr2, sizeof(server_addr2))) < 0){
+            //Implement error handling here
+			std::cout << "failed to connect to data socket - client error\n";
+        }
+		
+		//Get connect succesful message
+        if((read(dataSock, receiveBuff, sizeof(receiveBuff) - 1)) < 0){
+			//Implement error handling here
+			std::cout << "failed to read from socket\n";
+		}
+        write(fileno(stdout), receiveBuff, sizeof(receiveBuff) - 1);	//Write server response to stdout
+		
+		memset(receiveBuff, 0, SIZE);	//Clear receiveBuff
 
-    close(origSock);
+		//Set data transfer type - ie. binary
+		write(commSock, trfType, sizeof(trfType));
+		if((read(commSock, receiveBuff, sizeof(receiveBuff) - 1)) < 0){
+			//Implement error handling here
+			std::cout << "failed to read from socket after data type set\n";
+		}
+		write(fileno(stdout), receiveBuff, sizeof(receiveBuff) - 1);	//Write server response to stdout
+		
+		memset(receiveBuff, 0, size);	//Clear receiveBuff
+
+		//Create command with file name to transfer
+		strcat(getBuff, fileName);
+		strcat(getBuff, "\r\n");
+
+		write(commSock, getBuff, strlen(sendBuff));
+		if((read(commSock, receiveBuff, sizeof(receiveBuff))) < 0){
+			//Implement error handling here
+			cout << "Failed to read from command socket after GET command send\n";
+		}
+
+		read(dataSock, receiveBuff, sizeof(receiveBuff));
+
+		ofstream xmlfile;
+		xmlfile.open(fileName);
+		xmlfile << receiveBuff;
+		xmlfile.close();
+
+		close(dataSock);
+		close(commSock);
+    }
+
+	//Clean up
+	free(server_addr);
+	free(server_addr2);
 
     return 0;
 }
